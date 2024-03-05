de24b5591d422ceea0425c9d2d065a5ec2598789
nishant129 <nishantborkar139@gmail.com>
2024-03-05 20:43:04 +0530

    add first stage for data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.mlploject import logger
+from src.mlploject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logging is ok")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
